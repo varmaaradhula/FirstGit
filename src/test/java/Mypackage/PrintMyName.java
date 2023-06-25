05deecb3e04f6078cdb6dbfe311aa19993bd7a0c
Varma <rkvarmaa@gmail.com>
2023-06-25 22:44:26 +0100

    user X second commit

--- src/test/java/Mypackage/PrintMyName.java
+++ src/test/java/Mypackage/PrintMyName.java
@@ -9,9 +9,17 @@ public class PrintMyName {
 
 		String wifename = " Jyothsna Bendhe";
 		
+		String daughtername = "Preesha Aradhula";
+		
+		String sonname = "Ayansh Aradhula";
+		
 		System.out.println(" my name is : "+myname);
 
 		System.out.println(" my wife name is : "+wifename);
+		
+		System.out.println(" my daughter name is : "+daughtername);
+		
+		System.out.println(" my son name is : "+sonname);
 
 	}
 
