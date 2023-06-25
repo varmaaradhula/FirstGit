c7e0b073dd04b61410a259762e9540bab279ce56
Varma <rkvarmaa@gmail.com>
2023-06-25 22:13:06 +0100

    first commit by X

---
+++ src/test/java/Mypackage/PrintMyName.java
@@ -0,0 +1,14 @@
+package Mypackage;
+
+public class PrintMyName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String myname = "Radha Krishna Varma Aradhula";
+		
+		System.out.println(" my name is : "+myname);
+
+	}
+
+}
